19391d104018b81c6e711fa89d89f11f67f681ef
Sowndarya-2024 <sowndaryagowda21@gmail.com>
2024-08-13 13:52:33 +0530

    Update 01_01_TypeCasting.py

--- week02/01_01_TypeCasting.py
+++ week02/01_01_TypeCasting.py
@@ -3,11 +3,15 @@
 a = 7
 
 # 1. print the type of the variable
+print(type(a))
 #   Convert integer variable to float and confirm the type cast worked (print it out)
-
+b=float(a)
+print(b)
 
 # 2. Now, Convert your float variable to string and print out the type
+c=string(b)
+print(c)
 
 
 # 3. Finally, Convert your string variable back to integer and print it out (the type)
-
+print(int(c))
